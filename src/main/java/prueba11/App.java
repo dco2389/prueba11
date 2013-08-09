d9569582a2a6c16b2294d95066e2f9d78672cd28
daniel cardona <dco-89@hotmail.com>
2013-08-09 11:01:56 -0500

    Inicio repositorio

---
+++ src/main/java/prueba11/App.java
@@ -0,0 +1,13 @@
+package prueba11;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
